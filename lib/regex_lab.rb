dc443dd695cfc85f3355cca2ca5bb80c88204d07
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-23 02:51:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]+\w/)then
+    return true 
+  else 
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
+  text.match(/\w+{5}/)
 
 end
 
